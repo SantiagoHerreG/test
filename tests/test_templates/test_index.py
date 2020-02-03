856b9f0b973543bc8d228098cbc4f26cf69160b1
Santiago Herrera <888@holbertonschool.com>
2020-02-03 03:08:40 +0000

    Tests cases for index display, using selenium. Not validated

--- tests/test_templates/test_index.py
+++ tests/test_templates/test_index.py
@@ -11,15 +11,13 @@ class Test_index(unittest.TestCase):
     def setUp(self):
         """ New set up clase for every test
         """
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
         self.driver.get("https://tusavirus.herokuapp.com/")
-
 
     def tearDown(self):
         """Method to be executed after each test
         """
         self.driver.quit()
-
 
     def test_submit(self):
         """ Tests if a new element with correct input is correctly submitted
@@ -33,3 +31,29 @@ class Test_index(unittest.TestCase):
 
         continue_link = self.driver.find_element_by_link_text('Submit')
         self.assertTrue("New jurisdiction added succesfully" in continue_link)
+
+    def test_submit_wrong_jurisdiction(self):
+        """ Tests if a post request contains a non valid jurisdiction
+        """
+        self.jurisdictions_field = self.driver.find_element_by_name("name")
+        self.sick_people_field = self.driver.find_element_by_name("victims")
+
+        self.jurisdictions_field.send_keys("Hello")
+        self.sick_people_field.send_keys("1 million")
+        self.sick_people_field.submit()
+
+        continue_link = self.driver.find_element_by_link_text('Submit')
+        self.assertTrue("Not a valid jurisdiction" in continue_link)
+
+    def test_submit_wrong_value(self):
+        """ Tests if a post request contains a non valid number
+        """
+        self.jurisdictions_field = self.driver.find_element_by_name("name")
+        self.sick_people_field = self.driver.find_element_by_name("victims")
+
+        self.jurisdictions_field.send_keys("Antioquia")
+        self.sick_people_field.send_keys("dog")
+        self.sick_people_field.submit()
+
+        continue_link = self.driver.find_element_by_link_text('Submit')
+        self.assertTrue("Not a valid number" in continue_link)
